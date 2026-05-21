5a7f43f5c57f3d78ba2680c66a25a905b965981a
Fersh <321287041@comunidad.unam.mx>
2026-05-21 08:17:00 -0600

    Cambio en MainActivity.kt para que se muestre correctamente en la app

--- app/src/main/java/com/example/actividad6/MainActivity.kt
+++ app/src/main/java/com/example/actividad6/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
